58a3a13e17723e8037a988c134abb87ac7f3a5c4
Alejandro Sancho <34617@DESKTOP-QUD65H0>
2021-02-21 13:41:30 +0100

    Docuentar es de sabios, aunque leerla sea pesado

---
+++ geometria/ladocu/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CirculoASI"},{"p":"<Unnamed>","l":"FiguraGeometricaASI"},{"p":"<Unnamed>","l":"RectanguloASI"},{"p":"<Unnamed>","l":"TrianguloASI"}]
